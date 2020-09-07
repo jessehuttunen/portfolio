0ad1db8e50ba300e3dc6c4ff476c61310dc0d33d
jessehuttunen <31622099+jessehuttunen@users.noreply.github.com>
2020-09-07 11:05:41 +0300

    dff

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.23f7e589ff521b16ee419c7c14680d6e.js"
+  "/portfolio/precache-manifest.31e8521cf3063ea58e23251537d03c9d.js"
 );
 
 self.addEventListener('message', (event) => {
